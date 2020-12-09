01a3ce5b21579db731e8a8bd19a9e3dc5790ddd8
Zack <altschulerzack@yahoo.com>
2020-12-08 19:43:56 -0800

    cors

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use(function errorHandler(error, req, res, next) {
 });
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
